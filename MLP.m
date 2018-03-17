e348af7ec31b2766f2ecf9dafc8fd12e53a7c8ca
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-2.local>
2018-03-17 17:33:26 -0300

    Updated weights

--- MLP.m
+++ MLP.m
@@ -1,6 +1,6 @@
 1;
 
-function w = backpropagation(bits, patterns, targets, activationFunc, layers)
+function w = backpropagation(bits, patterns, targets, activationFunc, layers, learningRate)
 
 	bias = -1*ones(pow2(bits),1);
 	patterns = [bias, patterns];
@@ -23,9 +23,9 @@ function w = backpropagation(bits, patterns, targets, activationFunc, layers)
 
 	numberOfLayers = rows(layers);
 
-	h = cell(numberOfLayers,1);
+	h = cell(numberOfLayers,1); %hi
 
-	v = cell(numberOfLayers,1);
+	v = cell(numberOfLayers,1); 
 	
 	for m = 1:numberOfLayers
 		
@@ -45,25 +45,37 @@ function w = backpropagation(bits, patterns, targets, activationFunc, layers)
 
 % 4. Calculate deltas between output layer and wanted output
 
-	deltas = cell(numberOfLayers,1);
+	delta = cell(numberOfLayers,1);
 
 	delta{numberOfLayers} = v{numberOfLayers}*(1-v{numberOfLayers}) * (target-v{m});
-
+  
 
 % 5. Calculate deltas for previous layers propagating errors backwards for each neuron
 % of each layer. m = M, M - 1, ..., 2
 % TODO: delta de umbrales no se calcula cr
+% 6. Update all weights
 
 	for m = numberOfLayers:2
 		
-		delta{m-1} = v{m}*(1-v{m})*(w{m}(i)*delta{m-1});
-
+		delta{m-1} = v{m}*(1-v{m})*(w{m}(i)*delta{m});
+    
+    for i= 1:rows(w{m}) 
+      for j= 1:columns(w{m})
+          deltaW = learningRate * delta{m}(i) * v{m-1}(j);
+          w{m}(i,j) = w{m}(i,j) + deltaW;
+      endfor
+    endfor
 	endfor
-
-
-% 6. Update all weights
+  
+  for i= 1:rows(w{1}) 
+      for j= 1:columns(w{1})
+          deltaW = learningRate * delta{1}(i) * inputPattern(j);
+          w{1}(i,j) = w{1}(i,j) + deltaW;
+      endfor
+    endfor
+  
 % 7. Back to step 2 for next pattern
-endfor
+%endfor
 
 endfunction
 
@@ -71,5 +83,5 @@ function w = mlp(bits, learningRate, activationFunc, operation, limitEpochs, tra
 	
 
 
-	w = backpropagation(bits, trainingSet(0), trainingSet(1), activationFunc, layers);
+	w = backpropagation(bits, trainingSet(0), trainingSet(1), activationFunc, layers, learningRate);
 endfunction
