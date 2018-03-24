4e63b523056acfe6dfbf84654eafdeaaa4c36427
j1nma <j1nma@users.noreply.github.com>
2018-03-24 04:43:46 -0300

    added missing conflict resolution

--- config/xorConfig.m
+++ config/xorConfig.m
@@ -14,10 +14,6 @@ trainingType = 'incremental';
 
 momentum = 0.9;
 
-<<<<<<< HEAD
 adaptativeLearningRate = 1;
 
 limitEpochsForLearningRate = 10;
-=======
-adaptativeLearningRate = 1; %1 para si. 0 para no
->>>>>>> ded4728a4677f5bb71c589d0d1908a5af25ed685
